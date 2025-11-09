0875d140925ffe2fbe90b78f30392b8980abf130
ruby <x.rubycryst.x@gmail.com>
2025-11-09 12:47:29 +0000

    Main

--- threads.h
+++ threads.h
@@ -17,36 +17,27 @@
 namespace Photon
 {
 
-    typedef enum threadprivilege_t
-    {
-
-        MAINGRAPHICSTHREAD,     /* The main OpenGL Graphics thread */
-        GRAPHICSSUBTHREAD,      /* Sub-thread that submits graphics requests through the queue */
-        OBJECTIVETHREAD,        /* Thread where a game object's code is run on */
-        SUBJECTIVETHREAD        /* Manually invoked thread */
-
-    } threadprivilege_t;
+    //////////////////////////////////////////////////////
+    // The 'threads' class keeps track of all threads
+    //////////////////////////////////////////////////////
 
     class EThreads
     {
 
-        typedef struct CThread
-        {
-
-            std::thread mThread;
-            threadprivilege_t mPrivilege;
-
-        } CThread;
+    
 
         public:
 
             EThreads();
+
+
+            
             
             virtual ~EThreads();
 
         private:
 
-            std::vector<std::thread> mThreads;
+            std::vector<> mThreads;
 
     };
 
